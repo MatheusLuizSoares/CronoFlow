5ae84156b2d06882c23a57e00bd7c22799e62148
Matheus Luiz Soares <matheusluiz15a@gmail.com>
2023-12-25 18:52:43 -0300

    consegui ambientar html com css

--- calendario/urls.py
+++ calendario/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/', views.home, name="home"),
-    path('agenda/', views.agenda, name="agenda"),
+    path('', views.home, name='home'),
+    path('agenda/', views.agenda, name='agenda'),
 ]
