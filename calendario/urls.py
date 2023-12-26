48ebd6ca1cbe85a1333064d4f3b15210b2498219
Matheus Luiz Soares <matheus.luiz1@academico.ifrn.edu.br>
2023-12-26 20:14:59 -0300

    adicionando mais funcionalidade

--- calendario/urls.py
+++ calendario/urls.py
@@ -1,8 +1,12 @@
 # calendario/urls.py
+from unicodedata import name
+# calendario/urls.py
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('agenda/', views.agenda, name='agenda'),
+    path('registro/', views.registro, name="registro"), 
+    path('login/', views.login, name="LOGIN")  
 ]
