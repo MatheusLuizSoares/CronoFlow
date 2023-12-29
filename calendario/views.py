2686f431786a633455aa118efc249e7d55922fad
Matheus Luiz Soares <matheusluiz15a@gmail.com>
2023-12-29 12:01:20 -0300

    verificando o usuario

--- calendario/views.py
+++ calendario/views.py
@@ -1,4 +1,5 @@
 # calendario/views.py
+from django.contrib.auth.models import User
 from django.shortcuts import render
 from django.http import HttpResponse
 
@@ -16,7 +17,14 @@ def registro(request):
      nome=request.POST.get("name")   
      email=request.POST.get("email")  
      senha=request.POST.get("senha")
-     return HttpResponse
+     
+     user=User.objects.filter(nome=nome).first()
+     if user:
+         return HttpResponse("já existe um usuario com esse nome")
+     
+     user = User.objects.create_user(username=nome, email=email, password=senha)  
+     
+     return HttpResponse("usuario cadastrado com sucesso")
 # Se precisar, você também pode adicionar a função de login aqui
 def login(request):
 
