bf49d4aa342aafc24a4b4d874e64b259bd7560d2
Matheus Luiz Soares <matheusluiz15a@gmail.com>
2023-12-29 13:06:28 -0300

    login senddo trabalhado

--- calendario/views.py
+++ calendario/views.py
@@ -2,6 +2,7 @@
 from django.contrib.auth.models import User
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth import authenticate,login
 
 def home(request):
     return render(request, "home.html")
@@ -18,15 +19,30 @@ def registro(request):
      email=request.POST.get("email")  
      senha=request.POST.get("senha")
      
-     user=User.objects.filter(nome=nome).first()
+     user=User.objects.filter(username=nome).first()
      if user:
          return HttpResponse("já existe um usuario com esse nome")
      
      user = User.objects.create_user(username=nome, email=email, password=senha)  
      
      return HttpResponse("usuario cadastrado com sucesso")
-# Se precisar, você também pode adicionar a função de login aqui
-def login(request):
 
+def login(request):
+ if request.method=="GET":
     return render(request, "login.html")
+ else:
+     username= request.POST.get("username")
+     senha= request.POSt.get("senha")
+     
+     user=authenticate(username=username, password=senha)
+     
+     if user:
+         login(request,user)
+         return HttpResponse("autenticado")
+     else:
+         return HttpResponse("Email ou senha invalidos")
+     
+def plataforma(resquest):
+    return HttpResponse("plataforma")
+    
 
